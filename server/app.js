67fad8b7bbd808468a47fbc804c2f9674d6ae2c3
Jay442 <jaygw1188@gmail.com>
2025-11-16 23:57:48 -0500

    heroku fix

--- server/app.js
+++ server/app.js
@@ -31,7 +31,7 @@ redisClient.on('error', (err) => console.log('Redis Client Error', err));
 redisClient.connect().then(() => {
   const app = express();
   app.use(helmet());
-  app.use('/assets', express.static(path.resolve(`${__dirname}/../hosted`)));
+  app.use(express.static(path.resolve(`${__dirname}/../hosted`)));
   app.use(favicon(`${__dirname}/../hosted/img/favicon.png`));
   app.use(compression());
   app.use(express.urlencoded({ extended: true }));

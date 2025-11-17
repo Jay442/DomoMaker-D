2be8ae18d0fc56ac4ffd669ee4f01a65c072bd32
Jay442 <jaygw1188@gmail.com>
2025-11-17 00:23:04 -0500

    revert fix

--- server/app.js
+++ server/app.js
@@ -31,8 +31,7 @@ redisClient.on('error', (err) => console.log('Redis Client Error', err));
 redisClient.connect().then(() => {
   const app = express();
   app.use(helmet());
-  app.use(express.static(path.resolve(`${__dirname}/../hosted`)));
-  app.use(favicon(`${__dirname}/../hosted/img/favicon.png`));
+  app.use('/assets', express.static(path.resolve(`${__dirname}/../hosted`)));
   app.use(compression());
   app.use(express.urlencoded({ extended: true }));
   app.use(express.json());
